4ac157f5215aa8546c3474ec7382b502468ec5e9
Shadowfied <admin@shadowfied.com>
2021-01-31 17:20:15 +0100

    build(purging): 🐛 don't purge css in dev mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,25 @@
 /* eslint-disable global-require */
 // postcss.config.js
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-	// Specify the paths to all of the template files in your project
-	content: [
-		'./pages/**/*.tsx',
-		'./components/**/*.tsx',
-		'./pages/**/*.ts',
-		'./pages/**/*.jsx',
-	],
+const purgecss =
+  process.env.NODE_ENV !== 'development'
+    ? require('@fullhuman/postcss-purgecss')({
+        // Specify the paths to all of the template files in your project
+        content: [
+          './pages/**/*.tsx',
+          './components/**/*.tsx',
+          './pages/**/*.ts',
+          './pages/**/*.jsx',
+        ],
 
-	// make sure css reset isnt removed on html and body
-	safelist: ['html', 'body'],
+        // make sure css reset isnt removed on html and body
+        safelist: ['html', 'body'],
 
-	// Include any special characters you're using in this regular expression
-	defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
+        // Include any special characters you're using in this regular expression
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      })
+    : null;
 
 module.exports = {
-	plugins: [require('tailwindcss')('./config/tailwind.config.js'), purgecss],
+  plugins: [require('tailwindcss')('./config/tailwind.config.js'), purgecss],
 };
